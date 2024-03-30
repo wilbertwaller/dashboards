fafa5dda1a0d0f5372faf9c90a89f55885b7d3cf
Wilbert Waller <wilbert.waller@gmail.com>
2024-03-29 23:35:17 -0600

    Init navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  title = '';
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.title = <string>this.route.snapshot.title ?? '';
+  }
+}
