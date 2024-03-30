c4a079722c576aa2c5e140b8e67edee1a42c5c7f
Wilbert Waller <wilbert.waller@gmail.com>
2024-03-29 23:36:19 -0600

    Add navbar to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
