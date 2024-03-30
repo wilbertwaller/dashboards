87cdcebf60ef7c607f45277cb216ec55723b8b9f
Wilbert Waller <wilbert.waller@gmail.com>
2024-03-29 23:37:37 -0600

    Init app route config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardManagerComponent } from './dashboard-manager/dashboard-manager.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent, title: 'Dashboards', data: { 'title': 'Dashboards' } },
+  { path: 'dashboard-manager', pathMatch: 'full', component: DashboardManagerComponent, title: 'Dashboard Manager', data: { 'title': 'Dashboard Manager' } }
+];
