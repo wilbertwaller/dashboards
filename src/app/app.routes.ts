1ae4b035000f26b3a2f324454667dddbce23f749
Wilbert Waller <wilbert.waller@gmail.com>
2024-03-30 18:14:11 -0600

    Add to route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,28 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardManagerComponent } from './dashboard-manager/dashboard-manager.component';
+import { UserManagerComponent } from './user-manager/user-manager.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent, title: 'Dashboards', data: { 'title': 'Dashboards' } },
-  { path: 'dashboard-manager', pathMatch: 'full', component: DashboardManagerComponent, title: 'Dashboard Manager', data: { 'title': 'Dashboard Manager' } }
+  {
+    path: '',
+    pathMatch: 'full', 
+    component: HomeComponent,
+    title: 'Dashboards',
+    data: { 'title': 'Dashboards' }
+  },
+  {
+    path: 'dashboard-manager', 
+    pathMatch: 'full',
+    component: DashboardManagerComponent,
+    title: 'Dashboard Manager',
+    data: { group: 'Admin' }
+  },
+  {
+    path: 'user-manager', 
+    pathMatch: 'full',
+    component: UserManagerComponent,
+    title: 'User Manager',
+    data: { group: 'Admin' }
+  }
 ];
