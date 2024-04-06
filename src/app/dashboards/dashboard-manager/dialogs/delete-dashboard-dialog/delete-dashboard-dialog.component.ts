2c45d7d225bb1bd3844c69764981569ffceaf08e
Wilbert Waller <wilbert.waller@gmail.com>
2024-04-05 22:17:36 -0600

    Add delete dashboard dialog

---
+++ src/app/dashboards/dashboard-manager/dialogs/delete-dashboard-dialog/delete-dashboard-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { Dashboard } from '../../../dashboard.model';
+import { DashboardService } from '../../../dashboard.service';
+
+@Component({
+  selector: 'app-delete-dashboard-dialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    MatIconModule
+  ],
+  templateUrl: './delete-dashboard-dialog.component.html',
+  styleUrl: './delete-dashboard-dialog.component.css'
+})
+export class DeleteDashboardDialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { dashboard: Dashboard },
+    private dialogRef: MatDialogRef<DeleteDashboardDialogComponent>,
+    private dashboardService: DashboardService
+  ) {}
+
+  deleteDashboard(): void {
+    this.dashboardService.removeDashboard(this.data.dashboard.id);
+    this.dialogRef.close(true);
+  }
+}
