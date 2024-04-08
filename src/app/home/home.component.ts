49a45daa8a048e95543133e9008cb909cd7f56ca
Wilbert Waller <wilbert.waller@gmail.com>
2024-04-07 19:49:03 -0600

    Update home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
@@ -7,6 +7,12 @@ import { concat, pick } from 'lodash';
 import { CommonModule } from "@angular/common";
 import { DashboardService } from "../dashboards/dashboard.service";
 import { Dashboard } from "../dashboards/dashboard.model";
+import { Subject, takeUntil } from "rxjs";
+
+interface Group {
+  name: string;
+  paths: Route[];
+}
 
 @Component({
   selector: 'app-home',
@@ -15,12 +21,28 @@ import { Dashboard } from "../dashboards/dashboard.model";
   templateUrl: './home.component.html',
   styleUrls: ['../shared/shared-styles.css', './home.component.css']
 })
-export class HomeComponent implements OnInit {
-  groups: { name: string; paths: Route[] }[] = [];
+export class HomeComponent implements OnInit, OnDestroy {
+  private onDestroy$ = new Subject<boolean>();
+  private unassociated = 'Unassociated';
+
+  groups: Group[] = [];
 
   constructor(private router: Router, private dashboardService: DashboardService) {}
 
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
   ngOnInit(): void {
+    const initialRoutes = this.buildInitialRoutes();
+    this.dashboardService.dashboards.pipe(takeUntil(this.onDestroy$)).subscribe((dashboards: Dashboard[]) => {
+      const additionalRoutes = this.buildAdditionalRoutes(dashboards);
+      this.groups = concat(initialRoutes, additionalRoutes);
+    });
+  }
+
+  private buildInitialRoutes(): Group[] {
     const groupMap = this.router.config.reduce((map, route) => {
       const key = route?.data?.['group'];
       if (key) {
@@ -30,18 +52,17 @@ export class HomeComponent implements OnInit {
       }
       return map;
     }, new Map<string, Route[]>());
-    const initialRoutes = Array.from(groupMap, ([name, paths]) => ({ name, paths }));
-    const unassociated = 'Unassociated';
-    this.dashboardService.dashboards.subscribe((dashboards: Dashboard[]) => {
-      const groupMap = dashboards.reduce((map, dashboard) => {
-        const key = dashboard.group || unassociated;
-        const routes = map.get(key) ?? [];
-        routes.push({ path: `dashboards/${dashboard.id}`, title: dashboard.name });
-        map.set(key, routes);
-        return map;
-      }, new Map<string, Route[]>());
-      const additionalRoutes = Array.from(groupMap, ([name, paths]) => ({ name, paths }));
-      this.groups = concat(initialRoutes, additionalRoutes);
-    })
+    return Array.from(groupMap, ([name, paths]) => ({ name, paths }));
+  }
+
+  private buildAdditionalRoutes(dashboards: Dashboard[]): Group[] {
+    const groupMap = dashboards.reduce((map, dashboard) => {
+      const key = dashboard.group || this.unassociated;
+      const routes = map.get(key) ?? [];
+      routes.push({ path: `dashboards/${dashboard.id}`, title: dashboard.name });
+      map.set(key, routes);
+      return map;
+    }, new Map<string, Route[]>());
+    return Array.from(groupMap, ([name, paths]) => ({ name, paths }));
   }
 }
