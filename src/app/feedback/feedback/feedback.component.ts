fe6dac79c8c499578e41147163c2e389876ecd08
Wilbert Waller <wilbert.waller@gmail.com>
2024-04-28 19:06:55 -0600

    Add sort to feedback table

--- src/app/feedback/feedback/feedback.component.ts
+++ src/app/feedback/feedback/feedback.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { Feedback } from '../feedback.model';
 import { FeedbackService } from '../feedback.service';
 import { Subject, takeUntil } from 'rxjs';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [MatTableModule, NavbarComponent],
+  imports: [MatSortModule, MatTableModule, NavbarComponent],
   templateUrl: './feedback.component.html',
-  styleUrl: './feedback.component.css'
+  styleUrls: ['../../shared/shared-styles.css', './feedback.component.css']
 })
-export class FeedbackComponent implements OnDestroy, OnInit {
+export class FeedbackComponent implements AfterViewInit, OnDestroy, OnInit {
   private isDestroyed$ = new Subject<boolean>();
 
   dataSource = new MatTableDataSource<Feedback>();
   displayedColumns: string[] = ['dashboardName', 'title', 'summary'];
 
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private feedbackService: FeedbackService) {}
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnDestroy(): void {
     this.isDestroyed$.next(true);
